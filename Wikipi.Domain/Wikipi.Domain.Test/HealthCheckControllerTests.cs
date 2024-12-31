ddee86f481083485987c7cc1d2cde16b2ce648fe
Sendilkumar Murugaiah <sendilkumarmurugaiah@gmail.com>
2024-12-31 21:42:16 +0530

    Domain api test project added.

---
+++ Wikipi.Domain/Wikipi.Domain.Test/HealthCheckControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Wikipi.Domain.Controllers;
+
+namespace Wikipi.Domain.Test
+{
+    [TestClass]
+    public sealed class HealthCheckControllerTests
+    {
+        [TestMethod]
+        public void VerifyHealthCheck_Get()
+        {
+            //Arrange
+            var controller = new HealthCheckController();
+
+            // Act
+            var result  = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+           
+        }
+    }
+}
